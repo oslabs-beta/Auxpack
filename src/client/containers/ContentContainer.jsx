1b5c483985738486034b2dcc90d392445d2a3ee5
Nobuhide Ajito <najito@Emis-MacBook.socal.rr.com>
2019-11-29 21:40:27 -0800

    basic frontend framework created, temp server created, webpack configured

---
+++ src/client/containers/ContentContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContentContainer = (props) => {
+    return ( 
+        <p>content</p>
+     );
+}
+ 
+export default ContentContainer;
---
+++ src/client/containers/NavbarContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavbarContainer = (props) => {
+    return ( 
+        <p>navbar</p>
+     );
+}
+ 
+export default NavbarContainer;
