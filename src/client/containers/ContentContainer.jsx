8592fa1f30a7033ad68046b7d688be8f9395b303
Connie <clai011@ucr.edu>
2019-12-02 15:15:08 -0800

    wrote fetch to /getStats to backend for aux-stats.json data

--- src/client/containers/ContentContainer.jsx
+++ src/client/containers/ContentContainer.jsx
@@ -3,21 +3,36 @@ import { Switch, Route } from 'react-router-dom'
 import Overview from '../content/containers/Overview.jsx'
 import BuildData from '../content/containers/BuildData.jsx'
 import Recommendations from '../content/containers/Recommendations.jsx'
-import mockStats from '../../mockData';
+// import mockStats from '../../mockData';
 const ContentContainer = (props) => {
-    const [mockData, setMockData] = useState([])
+    const [mockData, setMockData] = useState([]);
 
     useEffect(() => {
-        setMockData(mockStats)
+        //setMockData(mockStats)
+
+        fetch('/getStats')
+            .then(res => res.json())
+            .then(data => setMockData([...data]))
+            .catch(err => console.log(err))
     }, [])
+
     console.log(`mock data: `, mockData)
+
+    const [activeBuild, handleClick] = useState(mockData.length - 1);
+
+    const clickHandler = e => {
+        const length = mockData.length;
+        const i = length - e.target.getAttribute('data-build');
+        handleClick()
+    }
+
     return (
         <React.Fragment>
             <Switch>
                 <Route
                     exact path="/"
                     render={() => (
-                        <Overview mockData={mockData} />
+                        <Overview mockData={mockData} activeBuild={activeBuild} />
                     )}
                 />
                 <Route
