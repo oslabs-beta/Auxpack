ce9f8e4510d441923beaf0db8c1c445ac2c43bde
Connie <clai011@ucr.edu>
2019-12-02 14:48:08 -0800

    fake data array successfully displayed when useEffect is ran in ContentContainer

--- src/client/views/sunburst/Sunburst.jsx
+++ src/client/views/sunburst/Sunburst.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Sunburst extends Component {
     constructor() {
         super()
-        state = {}
+        this.state = {}
     }
     render() {
         return <div></div>
