3dc10fec19b5037f1591cc5124fef4413f91b136
Stephanie Chiu <stephaniekchiu@gmail.com>
2019-12-14 11:55:09 -0800

    include sample tests to ensure Jest and Enzyme are working

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,6 +1,7 @@
-import App from '../client/App.jsx';
-// see setupTests.js for config
+// Refer to setupTests.js for config
+import Overview from '../client/content/containers/Overview.jsx';
 
+// sample test to ensure Jest is working
 describe('Examining the syntax of Jest tests', () => {
    
   it('sums numbers', () => {
@@ -9,3 +10,32 @@ describe('Examining the syntax of Jest tests', () => {
    });
 });
 
+// sample test to ensure Enzyme is working
+describe('React unit tests', () => {
+  describe('Overview', () => {
+    let wrapper;
+
+    const props = {
+      build:[{
+      timeStamp:1575426090404,
+      time:1439,
+      hash:"546142ce1b49a6ba7d6f",
+      errors:[],
+      size:1172113,
+      assets:[{"name":"bundle.js","chunks":["main"],"size":1172113}],
+      chunks:[{"size":1118609,"files":["bundle.js"],"modules":[{"name":"./client/App.jsx","size":6375,"id":"./client/App.jsx"}]}],
+      treeStats:{csj:[], esm:[], both:[]}
+      }],
+      activeBuild: 1
+    }
+
+    beforeAll(() => {
+      wrapper = shallow(<Overview {...props} />);
+    });
+
+    it('Snapshot testing labeled test', () => {
+      expect(toJson(wrapper)).toMatchSnapshot();
+    })
+  })
+
+})
--- src/_tests_/setupTests.js
+++ src/_tests_/setupTests.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';;
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 // Make Enzyme functions available in all test files without importing
@@ -8,3 +9,4 @@ global.React = React;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
+global.toJson = toJson;
