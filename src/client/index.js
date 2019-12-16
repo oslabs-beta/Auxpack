ff2dfb3d719a0ac2074b87e5ad5758dd62d7c62f
Stephanie Chiu <stephaniekchiu@gmail.com>
2019-12-15 17:11:18 -0800

    move scss file from App to index

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 import { BrowserRouter } from "react-router-dom";
+import '../assets/css/styles.scss';
 
 render(
     (//BrowerRouter initializes react router for the entirety of the application
