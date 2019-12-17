8379e0acf47e9c90c3dcd76d070da3121b251023
najito <nobuhide95@gmail.com>
2019-12-17 12:15:05 -0800

    Deleted test that doesn't function

--- __tests__/App.js
+++
@@ -1,9 +0,0 @@
-import BottomNavigation from '../src/client/components/BottomNavigation.jsx'
-
-describe('React tests', () => {
-    it('renders APP', () => {
-        let wrapper = createMount(<BottomNavigation/>)
-        expect(wrapper.find({ label: "Overview"})).exists().toBe(true)
-    })
-    
-})
