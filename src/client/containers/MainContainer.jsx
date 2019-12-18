3451a9c831f57c7c1a922b2243f1718752404615
Connie <clai011@ucr.edu>
2019-12-18 09:12:10 -0800

    unit tests passed

--- src/client/containers/MainContainer.jsx
+++ src/client/containers/MainContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import NavbarContainer from './NavbarContainer';
 import ContentContainer from './ContentContainer'
 
